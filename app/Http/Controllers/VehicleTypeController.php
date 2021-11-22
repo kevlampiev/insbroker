a42a653002489ec00eb6332441cd87f69552a61e
NikolausA <nik.aleksenko@gmail.com>
2021-11-22 17:17:18 +0000

    добавил компонент с формой для отправки данных заказчика

--- app/Http/Controllers/VehicleTypeController.php
+++ app/Http/Controllers/VehicleTypeController.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 class VehicleTypeController extends Controller
 {
-    //
+    // public function index()
+    // {
+    //     return view('customer.VehicleInsuranceRequest');
+    // }
 }
--- resources/views/customer/VehicleInsuranceRequest.blade.php
+++ resources/views/customer/VehicleInsuranceRequest.blade.php
@@ -7,14 +7,15 @@
 
 
 @section('content')
-
+<main id="main">
     <section id="request" class="request">
-        <br>
-        <hr>
-        <div class="container">Внесите данные владельца трaнспортного средства
+        <div class="container mt-2">
+            <h2 class="text-center text-decoration-underline">Запрос на страхование техники</h2>
+            <h4 class="text-center mt-3">Внесите данные владельца трaнспортного средства</h4>
             <div class="row text-center">
                 <div class="col-lg-8 offset-lg-2">
                     <form action="" method="post" role="form" class="php-email-form">
+
                         <div class="form-row">
                             <div class="col form-group">
                                 <input type="text" name="name" class="form-control" id="name" placeholder="Ваше имя"
@@ -35,6 +36,7 @@
                             </div>
 
                         </div>
+                        <h4 class="mt-3">Ваши контактрые данные для направления предложений</h4>
                         <div class="form-row">
                             <div class="col form-group">
                                 <input type="text" name="phone" class="form-control" id="phone"
@@ -54,17 +56,27 @@
                                 <div class="validate"></div>
                             </div>
                         </div>
-                        <div class="form-row">
-                            <div class="col-4 form-group">
-                                <select class="form-control">
-                                    <option selected>Выберите тип страховки</option>
-                                    <option value="1">ОСАГО</option>
-                                    <option value="2">КАСКО</option>
-                                    <option value="3">Полный фарш</option>
-                                </select>
+                        <h4 class="mt-3">Параметры страховки</h4>
+                        <div class="form-row mb-5">
+                            <img src="https://picsum.photos/150/150/?image=3" class="rounded float-start" alt="...">
+                            {{-- <img
+                                @if($vehicle->img_file)
+                                src="{{asset(config('paths.vehicles.get','storage/img/vehicles/').$vehicle->img_file)}}" class="card-img-top" alt="..."
+                                @else
+                                src="{{asset('storage/img/no_image_found.jpeg')}}" class="card-img-top" alt="..."
+                                @endif
+                            id="img-viewer"> --}}
+                            <div class="col form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="osago">
+                                <label class="form-check-label" for="osago">
+                                    ОСАГО
+                                </label>
                             </div>
-                            <div class="col-8 form-group">
-                                <input type="file" class="form-control" name="pts">
+                            <div class="col form-check">
+                                <input class="form-check-input" type="checkbox" onchange="changeVisible()" value="" id="kasko">
+                                <label class="form-check-label" for="kasko">
+                                    КАСКО
+                                </label>
                             </div>
                         </div>
                         <div class="form-row">
@@ -83,16 +95,10 @@
                             </div>
 
                         </div>
-                        {{-- <button type="button" class="btn btn-light" onclick="changeVisible()">Add frinchise</button> --}}
-                        @if (1)
-                            <div class="form-check form-check-inline form-row">
-                                <input class="form-check-input" type="checkbox" value="" id="choose-franchise">
-                                <label class="form-check-input mb-1" for="choose-franchise">
-                                    Использовать франшизу
-                                </label>
-                            </div>
-                            <div class="col-4 form-group">
-                                <input type="number" class="form-control" min="1000" step="500" name="franchise-value"
+                        <div class="addition-features" style="display: none;" id="addition">
+                            <label for="franchise-value">Укажите размер франшизы</label>
+                            <div class="col-4 form-group ml-0 p-0">
+                                <input type="number" class="form-control" id="franchise-value" min="1000" step="500" name="franchise-value"
                                        value="1000">
                             </div>
                             <div class="form-group">
@@ -100,35 +106,45 @@
                                       data-msg="Please write something for us" placeholder="Перечень рисков"></textarea>
                                 <div class="validate"></div>
                             </div>
-                        @endif
-
-                        @if ($errors->has('message'))
-                            <div class="mb-3">
-                                <div class="loading">Пересылаю ...</div>
-                                <div class="error-message"></div>
-                                <div class="sent-message">Мы получили Ваше сообщение. Спасибо!</div>
-                            </div>
-                        @endif
-
+                        </div>
                         <div class="text-center">
-                            <button type="submit">Отправить</button>
+                            <button type="button" class="btn btn-danger">Отправить</button>
                         </div>
                     </form>
                 </div>
 
             </div>
         </div>
-        <hr>
-        <br>
     </section>
+</main>
 
-    {{--<script>--}}
-    {{--    let isVisible = false--}}
-    {{--    changeVisible(e)--}}
-    {{--    {--}}
-    {{--        console.log(isVisible);--}}
-    {{--        isVisible = !isVisible--}}
-    {{--    }--}}
-    {{--</script>--}}
 
+
+    <script>
+    let checkbox = document.getElementById('kasko');
+    checkbox.addEventListener('change', function changeVisible() {
+        if (this.checked) {
+            document.getElementById('addition').style.display = "block";
+        } else {
+            document.getElementById('addition').style.display = "none";
+        }
+    });
+
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function(e) {
+            $('#img-viewer').attr('src', e.target.result);
+            $('#pts_tmp_path').attr('value', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#inputGroupFile01").change(function() {
+        readURL(this);
+    });
+
+    </script>
 @endsection
